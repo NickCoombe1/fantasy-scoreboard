244d0baa530d6a53ee2c00aa28f0f430f083fadf
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-12-10 17:26:43 +0000

    Fix text style

--- app/components/scoring/scoreboardHeader.tsx
+++ app/components/scoring/scoreboardHeader.tsx
@@ -16,21 +16,21 @@ export default function ScoreBoardHeader({
       >
         {alignPoints === "left" && (
           <div className="w-[3.375rem] h-[3.375rem] md:h-[4.4rem] px-6 md:px-10 py-6  bg-light-default dark:bg-white  rounded-2xl border border-white/50 justify-center items-center flex self-start mx-4 md:mx-0">
-            <div className="text-center text-[#eae8f0] dark:text-light-90 text-base md:text-[1.625rem] font-semibold font-roobert leading-[.25rem] md:leading-normal">
+            <div className="text-center text-[#eae8f0] dark:text-light-90 text-base md:text-[1.625rem] font-semibold font-roobert leading-[.875rem] md:leading-normal">
               {" "}
               {totalPoints}
             </div>
           </div>
         )}
         <div className="grow shrink basis-0 h-[3.375rem] w-[10.25rem] md:h-[4.4rem] px-6 md:px-10 py-5 bg-white/70 dark:bg-white/5 rounded-2xl border border-white/50 justify-center items-center flex">
-          <div className="text-center  dark:text-dark-90 text-light-90 text-base md:text-[1.625rem] font-semibold font-roobert leading-[.25rem] md:leading-normal">
+          <div className="text-center  dark:text-dark-90 text-light-90 text-base md:text-[1.625rem] font-semibold font-roobert leading-[.875rem] md:leading-normal">
             {" "}
             {teamName}
           </div>
         </div>
         {alignPoints === "right" && (
           <div className="w-[3.375rem] h-[3.375rem] md:h-[4.4rem] px-6 md:px-10 py-6  bg-light-default dark:bg-white  rounded-2xl border border-white/50 justify-center items-center flex self-end mx-4 md:mx-0">
-            <div className="text-center text-[#eae8f0] dark:text-light-90 text-base md:text-[1.625rem] font-semibold font-roobert leading-[.25rem] md:leading-normal">
+            <div className="text-center text-[#eae8f0] dark:text-light-90 text-base md:text-[1.625rem] font-semibold font-roobert leading-[.875rem] md:leading-normal">
               {" "}
               {totalPoints}
             </div>
