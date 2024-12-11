92641cfc2926cc8e9b9ed58af925ed20e57c2bd9
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-12-11 18:41:57 +0000

    fix header on select style

--- app/(scoring)/scoring/[leagueID]/[teamID]/page.tsx
+++ app/(scoring)/scoring/[leagueID]/[teamID]/page.tsx
@@ -91,32 +91,32 @@ export default function ScoringPage({
 
   return (
     <div className={"min-h-[80vh] flex flex-col items-center"}>
-      <TabGroup className={"w-full md:w-auto"}>
+      <TabGroup className={"w-full"}>
         <div className="w-full md:px-1 pt-8 md:pb-1 bg-black/5 dark:bg-black/20 rounded-lg shadow backdrop-blur-2xl flex-col justify-start items-center gap-8 inline-flex">
           <div className="text-center dark:text-dark-90 text-light-90 text-[1.625rem] font-semibold leading-normal">
             Hawcey Gang
           </div>
           <TabList className="w-full justify-evenly items-center gap-3.5 md:gap-2 inline-flex h-[41px]">
-            <Tab className="md:h-3.5 px-4 py-3.5 data-[selected]:bg-white data-[selected]:bg-button-light-secondary data-[selected]:dark:bg-button-dark-bg data-[selected]:dark:bg-button-dark-secondary data-[selected]:bg-blend-overlay data-[selected]:bg-transparent rounded justify-center items-center gap-2.5 inline-flex">
+            <Tab className="md:h-3.5 px-4 py-3.5 data-[selected]:bg-white data-[selected]:dark:bg-button-dark-bg data-[selected]:dark:bg-button-dark-secondary data-[selected]:bg-blend-overlay  rounded justify-center items-center gap-2.5 inline-flex">
               <div className="text-center text-light-default dark:text-dark-default text-sm font-roobertMono uppercase leading-3 tracking-tight">
                 scoring
               </div>
             </Tab>
-            <Tab className="md:h-3.5 px-4 py-3.5 data-[selected]:bg-white data-[selected]:bg-button-light-secondary data-[selected]:dark:bg-button-dark-bg data-[selected]:dark:bg-button-dark-secondary data-[selected]:bg-blend-overlay data-[selected]:bg-transparent rounded justify-center items-center gap-2.5 inline-flex">
+            <Tab className="md:h-3.5 px-4 py-3.5 data-[selected]:bg-white  data-[selected]:dark:bg-button-dark-bg data-[selected]:dark:bg-button-dark-secondary data-[selected]:bg-blend-overlay  rounded justify-center items-center gap-2.5 inline-flex">
               <div className="text-center text-light-default dark:text-dark-default text-sm font-roobertMono uppercase leading-3 tracking-tight">
                 Matchup
               </div>
             </Tab>
-            <Tab className="md:h-3.5 px-4 py-3.5 data-[selected]:bg-white data-[selected]:bg-button-light-secondary data-[selected]:dark:bg-button-dark-bg data-[selected]:dark:bg-button-dark-secondary data-[selected]:bg-blend-overlay data-[selected]:bg-transparent rounded justify-center items-center gap-2.5 inline-flex">
+            <Tab className="md:h-3.5 px-4 py-3.5 data-[selected]:bg-white data-[selected]:dark:bg-button-dark-bg data-[selected]:dark:bg-button-dark-secondary data-[selected]:bg-blend-overlay  rounded justify-center items-center gap-2.5 inline-flex">
               <div className="text-center text-light-default dark:text-dark-default text-sm font-roobertMono uppercase leading-3 tracking-tight">
                 League Scoring
               </div>
             </Tab>
           </TabList>{" "}
         </div>
-        <TabPanels>
+        <TabPanels className={"w-full"}>
           <TabPanel>
-            <Scoring leagueID={leagueID} teamID={teamID} />
+            <Scoring />
           </TabPanel>
           <TabPanel>Content 2</TabPanel>
           <TabPanel>Content 3</TabPanel>
