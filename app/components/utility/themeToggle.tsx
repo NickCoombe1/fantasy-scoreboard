b18b3b8e3efab5fed2d921438efd6686c8001902
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-12-12 16:28:06 +0000

    tidy toggle

--- app/components/utility/themeToggle.tsx
+++ app/components/utility/themeToggle.tsx
@@ -12,7 +12,7 @@ export default function ThemeToggle(): React.ReactNode {
   return (
     <div className="h-[43px] p-1 bg-graphics-light-depth dark:bg-graphics-dark-depth rounded-lg z-40 shadow-custom-light-header backdrop-blur-20 justify-start items-center gap-1 inline-flex">
       <div
-        className={`absolute top-1 left-2 w-[35px] h-[35px] ] px-3.5 py-3 z-2 rounded transform transition-transform duration-400 z-[-2] ${
+        className={`absolute top-1 left-2 w-[35px] h-[35px] ] px-3.5 py-3 z-2 rounded transform transition-transform duration-200 z-[-2] ${
           theme === "light"
             ? "translate-x-[-4px] bg-button-light-bg"
             : "translate-x-full bg-button-dark-bg bg-button-dark-secondary"
@@ -32,22 +32,3 @@ export default function ThemeToggle(): React.ReactNode {
     </div>
   );
 }
-
-/*
-
-       
-        
-        
-             <div className="bg-button-light-bg dark:bg-transparent w-[35px] h-[35px] px-3.5 py-3 rounded justify-center items-center gap-2.5 inline-flex ">
-        <Button onClick={toggleTheme}>
-          <LightMode mode={theme} />
-        </Button>
-      </div>
-      <div className="w-[35px] h-[35px] px-3.5 py-3 dark:bg-button-dark-bg dark:bg-button-dark-secondary rounded justify-center items-center gap-2.5 inline-flex">
-        <Button onClick={toggleTheme}>
-          <DarkMode mode={theme} />
-        </Button>
-      </div>
-    );
-
-*/
